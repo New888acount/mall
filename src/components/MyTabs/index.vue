ca2509800d8c79e2137be2da0ec24050dd22418b
inserveracount@gmail.com <inserveracount@gmail.com>
2025-12-11 19:10:53 +0800

    订单

---
+++ src/components/MyTabs/index.vue
@@ -0,0 +1,291 @@
+<!--
+  使用方法和vant的tab标签页组件一样
+  文档:https://vant-ui.github.io/vant/#/zh-CN/tab
+-->
+<template>
+  <div
+    :class="[
+      'my-tabs',
+      arrowShow && `my-tabs--arrow my-tabs--arrow--${arrowShowType}`,
+      lineFill && 'my-tabs--line-fill',
+      wrapTopLineShow && 'my-tabs--wrap-top-line',
+      wrapBottomLineShow && 'my-tabs--wrap-bottom-line',
+    ]"
+  >
+    <van-tabs
+      v-bind="$attrs"
+      :shrink="shrink"
+      :color="color"
+      :titleInactiveColor="titleInactiveColor"
+      :titleActiveColor="titleActiveColor"
+      :lineWidth="lineWidth"
+      :lineHeight="lineHeight"
+      ref="myTabs"
+    >
+      <slot></slot>
+      <!-- 下面这几个slot是tabs本身就有的 -->
+      <slot name="nav-left"></slot>
+      <slot name="nav-right"></slot>
+      <slot name="nav-bottom"></slot>
+    </van-tabs>
+
+    <!-- 左边箭头 -->
+    <div class="my-tabs__arrow my-tabs__arrow--left" @click="handleTabsNavScroll('0')">
+      <slot name="arrow-left">
+        <i class="iconfont icon-xiangzuo1"></i>
+      </slot>
+    </div>
+    <!-- 右边箭头 -->
+    <div class="my-tabs__arrow my-tabs__arrow--right" @click="handleTabsNavScroll('10000')">
+      <slot name="arrow-right">
+        <i class="iconfont icon-kongxin-right-copy"></i>
+      </slot>
+    </div>
+  </div>
+</template>
+<script setup>
+import { defineProps, onBeforeUnmount, onMounted, ref } from 'vue'
+const props = defineProps({
+  color: {
+    type: String,
+    default: '#fe771c',
+  },
+  // 标题颜色
+  titleInactiveColor: {
+    type: String,
+    default: '#646566',
+  },
+  // 选中时候标题颜色
+  titleActiveColor: {
+    type: String,
+    default: '#323233',
+  },
+  // 选中时候线的宽度
+  lineWidth: {
+    type: String,
+    default: '30px',
+  },
+  // 选中时候线的高度
+  lineHeight: {
+    type: String,
+    default: '2px',
+  },
+  // 默认设置为收缩默认
+  shrink: {
+    type: Boolean,
+    default: true,
+  },
+  // 选中时候线是否铺满tab底部（新增）
+  lineFill: {
+    type: Boolean,
+    default: false,
+  },
+  // 容器头部线是否显示(新增)
+  wrapTopLineShow: {
+    type: Boolean,
+    default: false,
+  },
+  // 容器底部线是否显示(新增)
+  wrapBottomLineShow: {
+    type: Boolean,
+    default: false,
+  },
+  // 是否显示箭头点击进行滚动（新增）
+  arrowShow: {
+    type: Boolean,
+    default: false,
+  },
+})
+const myTabs = ref(null)
+
+onMounted(() => {
+  let tabsEl = myTabs.value.$el
+  let tabsNav = tabsEl.querySelector('.van-tabs__nav')
+  arrowShowFn(tabsNav)
+  tabsNav.addEventListener('scroll', onTabsNavScroll, true)
+  addEventListener('resize', onResize, true)
+})
+
+onBeforeUnmount(() => {
+  let tabsEl = myTabs.value.$el
+  let tabsNav = tabsEl.querySelector('.van-tabs__nav')
+  tabsNav.removeEventListener('scroll', onTabsNavScroll, true)
+  removeEventListener('resize', onResize, true)
+})
+
+/* 监听van-tabs__nav滚动条用于判断辅助箭头如何显示 */
+const onTabsNavScroll = (e) => {
+  arrowShowFn(e.target)
+}
+
+/* 监听屏幕改变用于屏幕变化的时候van-tabs__nav滚动条可以滚动到选中的tab */
+let resizeTimer = null
+const onResize = () => {
+  if (resizeTimer) {
+    clearTimeout(resizeTimer)
+    resizeTimer = null
+  }
+  resizeTimer = setTimeout(() => {
+    if (!myTabs.value) return
+    let tabsEl = myTabs.value.$el
+    let tabsNav = tabsEl.querySelector('.van-tabs__nav')
+    let tabActive = tabsEl.querySelector('.van-tab--active')
+    tabsNav.scrollLeft = tabActive.offsetLeft
+  }, 100)
+}
+
+/* 点击箭头滚动条用于快速滚动到左边或右边*/
+const handleTabsNavScroll = (value) => {
+  if (!myTabs.value) return
+  let tabsEl = myTabs.value.$el
+  let tabsNav = tabsEl.querySelector('.van-tabs__nav')
+  tabsNav.scrollTo({ left: value, behavior: 'smooth' })
+}
+
+/* none 不显示箭头，right显示右边箭头,left显示左边箭头，both显示两边箭头 */
+const arrowShowType = ref('none')
+let arrowShowTimer = null
+const arrowShowFn = (tabsNav) => {
+  if (!props.arrowShow) return
+  if (arrowShowTimer !== null) {
+    clearTimeout(arrowShowTimer)
+    arrowShowTimer = null
+  }
+  arrowShowTimer = setTimeout(() => {
+    if (tabsNav.scrollWidth === tabsNav.clientWidth) {
+      arrowShowType.value = 'none'
+    } else if (tabsNav.scrollLeft === 0) {
+      arrowShowType.value = 'right'
+    } else if (tabsNav.scrollLeft === tabsNav.scrollWidth - tabsNav.clientWidth) {
+      arrowShowType.value = 'left'
+    } else {
+      arrowShowType.value = 'both'
+    }
+  }, 100)
+}
+</script>
+
+<style lang="less" scoped>
+html[data-device='mobile'] {
+  .my-tabs {
+    position: relative;
+    width: 100%;
+    &--arrow {
+      :deep(.van-tabs__nav) {
+        padding: 0;
+        .van-tab {
+          font-size: 12px;
+          font-weight: 500;
+        }
+      }
+    }
+  }
+}
+.my-tabs {
+  position: relative;
+  width: 100%;
+
+  &__arrow {
+    position: absolute;
+    z-index: 9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+    &--left {
+      display: none;
+      top: 12px;
+      left: 2px;
+    }
+    &--right {
+      display: none;
+      top: 12px;
+      right: 2px;
+    }
+  }
+  &--arrow {
+    i {
+      font-size: 18px;
+    }
+    :deep(.van-tabs__wrap) {
+      padding: 0 24px;
+    }
+    &--none {
+      .van-tabs__wrap {
+        padding: 0;
+      }
+    }
+    &--left {
+      .my-tabs__arrow--left {
+        display: flex;
+      }
+    }
+    &--right {
+      .my-tabs__arrow--right {
+        display: flex;
+      }
+    }
+    &--both {
+      .my-tabs__arrow--left {
+        display: flex;
+      }
+      .my-tabs__arrow--right {
+        display: flex;
+      }
+    }
+  }
+  :deep(.van-tab) {
+    font-weight: 400;
+    padding: 0 24px;
+  }
+  :deep(.van-tab--active) {
+    font-weight: 700;
+    background: var(--desgin-gradient-common-bg2);
+  }
+
+  :deep(.van-tabs__nav--line) {
+    padding-bottom: 0;
+  }
+  &--line-fill {
+    // :deep(.van-tab--active) {
+    //   &::after {
+    //     display: none;
+    //     content: '';
+    //     position: absolute;
+    //     left: 0;
+    //     left: 8px;
+    //     right: 8px;
+    //     bottom: 0;
+    //     height: 2px;
+    //     border-radius: 99px;
+    //     background: #fe771c;
+    //   }
+    // }
+    :deep(.van-tabs__line) {
+      bottom: 0;
+    }
+  }
+  &--wrap-top-line {
+    :deep(.van-tabs__wrap) {
+      // border-top: 0.5px solid rgba(255, 255, 255, 0.1);
+    }
+  }
+  &--wrap-bottom-line {
+    :deep(.van-tabs__wrap) {
+      // border-bottom: 0.5px solid rgba(255, 255, 255, 0.1);
+    }
+  }
+  :deep(.van-tabs__nav--card) {
+    margin: 0 !important;
+  }
+  :deep(.van-tabs__wrap) {
+    min-height: 30px !important;
+    background: #fff;
+  }
+  :deep(.van-tabs__nav) {
+    background: none;
+  }
+}
+</style>
--- src/views/order/list.vue
+++ src/views/order/list.vue
@@ -1,81 +1,84 @@
 <template>
   <div class="order-list" v-loading="state.tabLoading">
-    <MobileHeader class="paysuccess-header" :title="$t('order.list.title')" :backicon="false"></MobileHeader>
+    <header>
+      <MobileHeader class="paysuccess-header" :title="$t('order.list.title')" :backicon="false"></MobileHeader>
 
-    <van-tabs v-model:active="activeName" @change="onTabChange">
-      <van-tab v-for="item in orderNav" :key="item.name" :title="item.text" :name="item.name">
-        <!-- {{ item.text }} -->
-        <MyPullRefreshList v-model:loading="state.loading" :finished="state.finished" @load="onLoad">
-          <div class="data-list">
-            <div class="data-item" v-for="item in state.list" :key="item.orderId">
-              <div class="data-top">
-                <div class="data-id">{{ $t('order.list.id') }}{{ item.orderId }}</div>
+      <MyTabs
+        v-model:active="activeName"
+        line-fill
+        wrapTopLineShow
+        wrapBottomLineShow
+        class="tabs"
+        @change="onTabChange"
+      >
+        <van-tab v-for="item in orderNav" :key="item.name" :title="item.text"></van-tab>
+      </MyTabs>
+    </header>
 
-                <div class="data-status">{{ getOrderStatusName(item.status) }}</div>
-              </div>
-              <div class="data-center">
-                <div class="order-item" v-for="obj in item.orderItemList" :key="obj.id">
-                  <div class="left">
-                    <MyImage
-                      class="box-img"
-                      v-if="$imgBaseUrl + obj.pic"
-                      :src="$imgBaseUrl + obj.pic"
-                      alt=""
-                      fit="initial"
-                    />
-                  </div>
-                  <div class="right">
-                    <div class="good-title">
-                      {{ obj.productName }}
-                    </div>
-                    <div class="good-tag">{{ obj.spDataValue }}</div>
-                    <div class="good-info">
-                      <div class="good-price">{{ obj.salePrice }}</div>
-                      <div class="good-qua">x {{ obj.quantity }}</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="data-sum">
-                {{ $t('order.list.sum', { count: item.orderItemList.length }) }}{{ item.payAmount }}
-              </div>
+    <MyPullRefreshList v-model:loading="state.loading" :finished="state.finished" @load="onLoad">
+      <div class="data-list">
+        <div class="data-item" v-for="item in state.list" :key="item.orderId">
+          <div class="data-top">
+            <div class="data-id">{{ $t('order.list.id') }}{{ item.orderId }}</div>
 
-              <div class="data-operate">
-                <div @click="detailHandle(item)" v-if="item.aftersaleStatus === 1">{{ $t('order.list.button1') }}</div>
-                <div
-                  v-if="item.status === 2 && item.aftersaleStatus === 1"
-                  @click="onConfirm(item)"
-                  class="continue-paying"
-                >
-                  {{ $t('order.list.button4') }}
+            <div class="data-status">{{ getOrderStatusName(item.status) }}</div>
+          </div>
+          <div class="data-center">
+            <div class="order-item" v-for="obj in item.orderItemList" :key="obj.id">
+              <div class="left">
+                <MyImage
+                  class="box-img"
+                  v-if="$imgBaseUrl + obj.pic"
+                  :src="$imgBaseUrl + obj.pic"
+                  alt=""
+                  fit="initial"
+                />
+              </div>
+              <div class="right">
+                <div class="good-title">
+                  {{ obj.productName }}
                 </div>
-                <div @click="canceltip(item)" v-if="item.status === 0">{{ $t('order.list.button2') }}</div>
-                <div
-                  @click="onPay({ orderSn: item.payId, totalAmount: item.totalAmount })"
-                  class="continue-paying"
-                  v-if="item.status === 0"
-                >
-                  {{ $t('order.list.button3') }}
+                <div class="good-tag">{{ obj.spDataValue }}</div>
+                <div class="good-info">
+                  <div class="good-price">{{ obj.salePrice }}</div>
+                  <div class="good-qua">x {{ obj.quantity }}</div>
                 </div>
-                <div
-                  @click="buyAgain(item)"
-                  v-if="item.status === 3 && item.aftersaleStatus === 1"
-                  class="continue-paying"
-                >
-                  {{ $t('order.list.button5') }}
-                </div>
-                <div @click="onDelete(item)" v-if="[0, 4, 5].includes(item.status)">{{ $t('order.list.button6') }}</div>
               </div>
             </div>
           </div>
-
-          <!-- 数据为空 -->
-          <div v-if="!state.list.length && !state.loading && !state.tabLoading">
-            <MyEmptyData />
+          <div class="data-sum">
+            {{ $t('order.list.sum', { count: item.orderItemList.length }) }}{{ item.payAmount }}
           </div>
-        </MyPullRefreshList>
-      </van-tab>
-    </van-tabs>
+
+          <div class="data-operate">
+            <div @click="detailHandle(item)" v-if="item.aftersaleStatus === 1">{{ $t('order.list.button1') }}</div>
+            <div
+              v-if="item.status === 2 && item.aftersaleStatus === 1"
+              @click="onConfirm(item)"
+              class="continue-paying"
+            >
+              {{ $t('order.list.button4') }}
+            </div>
+            <div @click="canceltip(item)" v-if="item.status === 0">{{ $t('order.list.button2') }}</div>
+            <div
+              @click="onPay({ orderSn: item.payId, totalAmount: item.totalAmount })"
+              class="continue-paying"
+              v-if="item.status === 0"
+            >
+              {{ $t('order.list.button3') }}
+            </div>
+            <div @click="buyAgain(item)" v-if="item.status === 3 && item.aftersaleStatus === 1" class="continue-paying">
+              {{ $t('order.list.button5') }}
+            </div>
+            <div @click="onDelete(item)" v-if="[0, 4, 5].includes(item.status)">{{ $t('order.list.button6') }}</div>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="!state.list.length && !state.loading && !state.tabLoading">
+        <MyEmptyData />
+      </div>
+    </MyPullRefreshList>
   </div>
 </template>
 
@@ -332,118 +335,144 @@ watch(
 </script>
 
 <style scoped lang="less">
-.data-list {
-  .data-item {
-    margin: 10px;
-    background: #fff;
-    padding: 0 10px;
-    border-radius: 5px;
+.order-list {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-end;
+  height: 100dvh;
+  header {
+    width: 100%;
+  }
 
-    .data-top {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      height: 40px;
-      line-height: 40px;
-      font-size: 13px;
-      .data-status {
-        color: #999;
-      }
-    }
-
-    .order-item {
-      display: flex;
-      justify-content: center;
-      padding: 10px 0;
-      border-bottom: 1px solid #f2f2f2;
-      .left {
-        margin-right: 8px;
-        .box-img {
-          width: 82px;
-          height: 82px;
-          margin-bottom: 8px;
-        }
-        .good-id {
-          color: #717378;
-          font-size: 12px;
-        }
+  .my-pull-refresh-list {
+    flex: 1;
+    overflow: scroll;
+    width: 100%;
+  }
+  .data-list {
+    padding: 10px;
+    width: 100%;
+    .data-item {
+      margin-bottom: 10px;
+      background: #fff;
+      padding: 0 10px;
+      border-radius: 5px;
+      &:last-child {
+        margin-bottom: 0;
       }
 
-      .right {
-        flex: 1;
-        .good-title {
-          overflow: hidden;
-          color: #1f2c3c;
-          text-overflow: ellipsis;
-          font-size: 14px;
-          font-weight: 500;
-          line-height: 150%;
-          display: -webkit-box;
-          -webkit-box-orient: vertical;
-          -webkit-line-clamp: 2;
-          align-self: stretch;
-          margin-bottom: 4px;
+      .data-top {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 40px;
+        line-height: 40px;
+        font-size: 13px;
+        .data-status {
+          color: #999;
         }
+      }
 
-        .good-tag {
-          color: #9d9ea2;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          font-size: 12px;
-          font-weight: 500;
-          line-height: 150%;
-          display: -webkit-box;
-          -webkit-box-orient: vertical;
-          -webkit-line-clamp: 1;
-          align-self: stretch;
-          margin-bottom: 4px;
-        }
-
-        .good-info {
-          font-size: 14px;
-          display: flex;
-          align-items: center;
-
-          .good-qua {
-            color: #999;
+      .order-item {
+        display: flex;
+        justify-content: center;
+        padding: 10px 0;
+        border-bottom: 1px solid #f2f2f2;
+        .left {
+          margin-right: 8px;
+          .box-img {
+            width: 82px;
+            height: 82px;
+            margin-bottom: 8px;
+          }
+          .good-id {
+            color: #717378;
             font-size: 12px;
-            margin-left: 4px;
+          }
+        }
+
+        .right {
+          flex: 1;
+          .good-title {
+            overflow: hidden;
+            color: #1f2c3c;
+            text-overflow: ellipsis;
+            font-size: 14px;
+            font-weight: 500;
+            line-height: 150%;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+            align-self: stretch;
+            margin-bottom: 4px;
+          }
+
+          .good-tag {
+            color: #9d9ea2;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            font-size: 12px;
+            font-weight: 500;
+            line-height: 150%;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 1;
+            align-self: stretch;
+            margin-bottom: 4px;
+          }
+
+          .good-info {
+            font-size: 14px;
+            display: flex;
+            align-items: center;
+
+            .good-qua {
+              color: #999;
+              font-size: 12px;
+              margin-left: 4px;
+            }
           }
         }
       }
-    }
 
-    .data-sum {
-      text-align: right;
-      margin-top: 15px;
-      font-size: 12px;
-      margin-bottom: 5px;
-    }
-
-    .data-operate {
-      text-align: center;
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      min-height: 50px;
-      flex-wrap: wrap;
-      div {
-        width: 80px;
-        height: 27.5px;
-        border-radius: 15px;
-        font-size: 13px;
-        border: 1px solid #dcdcdc;
-        line-height: normal;
-        margin-left: 5px;
+      .data-sum {
+        text-align: right;
+        margin-top: 15px;
+        font-size: 12px;
         margin-bottom: 5px;
-        line-height: 27.5px;
       }
 
-      .continue-paying {
-        background: linear-gradient(90deg, var(--color-light), rgba(255, 96, 0, 0.6));
-        color: #fff;
+      .data-operate {
+        text-align: center;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        min-height: 50px;
+        flex-wrap: wrap;
+        div {
+          width: 80px;
+          height: 27.5px;
+          border-radius: 15px;
+          font-size: 13px;
+          border: 1px solid #dcdcdc;
+          line-height: normal;
+          margin-left: 5px;
+          margin-bottom: 5px;
+          line-height: 27.5px;
+        }
+
+        .continue-paying {
+          background: linear-gradient(90deg, var(--color-light), rgba(255, 96, 0, 0.6));
+          color: #fff;
+        }
       }
     }
+  }
+
+  :deep(.my-empty-data) {
+    margin-top: 0;
+    background: transparent;
   }
 }
 </style>
