2f578eaeb5df2825f05023b208e180c87935be88
unionvv <testgoseed@gmail.com>
2025-12-13 13:35:19 +0800

    修改login函数文件

--- src/componentsFun/login/index.js
+++ src/componentsFun/login/index.js
@@ -1,35 +1,8 @@
-import { createApp } from 'vue'
-import LoginDialog from './index.vue'
-import setVueAppConfig from '@/utils/vueAppConfig'
-import useLocalCache from '@/hooks/storage/localStorage'
+import component from './index.vue'
+import AppMountComponent from '@/utils/appMountComponent.js'
+import useLocalCache from '@/hooks/storage/localStorage' // 假设这里能取 token
 
-function createLoginDialog(resolve, reject, options) {
-  // 创建一个节点，并将组件挂载上去
-  const mountNode = document.createElement('div')
-  document.body.appendChild(mountNode)
-  const app = createApp(LoginDialog, {
-    ...options,
-    show: true,
-    callback(res) {
-      // 避免重复挂载
-      if (window.LoginDialogVisible) {
-        window.LoginDialogVisible = false
-      }
-      app.unmount(mountNode) // 创建完后要进行销毁
-      document.body.removeChild(mountNode)
-      if (res && res?.access_token) {
-        resolve(res)
-      } else {
-        reject(false)
-      }
-    },
-  })
-  setVueAppConfig(app)
-  app.mount(mountNode)
-  window.LoginDialogVisible = true
-}
-
-export default function showLoginDialog(options) {
+export default function ShowLogin(options = {}) {
   return new Promise((resolve, reject) => {
     // 1. 判断是否已登录
     const token = useLocalCache().getCacheToken()
@@ -37,10 +10,18 @@ export default function showLoginDialog(options) {
       // 已登录，不弹窗，直接返回成功
       return resolve({ token })
     }
-    // 防止多次挂载
-    if (window.LoginDialogVisible) {
-      return resolve(true)
+    const finalParams = {
+      name: 'login-register',
+      component,
+      ...options,
+      callback(res) {
+        if (res) {
+          resolve(res) // 登录成功
+        } else {
+          reject(false) // 登录失败或取消
+        }
+      },
     }
-    createLoginDialog(resolve, reject, options)
+    new AppMountComponent(finalParams)
   })
 }
